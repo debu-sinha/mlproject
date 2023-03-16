cba25a19f014546f7d3f961303dc7f8f8b51ef05
Debu Sinha <debusinha2009@gmail.com>
2023-03-16 18:37:36 -0400

    fixed code in custom logger

--- src/logger.py
+++ src/logger.py
@@ -7,10 +7,10 @@ current_time = datetime.now()
 log_file_folder = current_time.strftime("%Y_%m_%d")
 log_file = f"{current_time.strftime('%H')}.log"
 
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE_FOLDER)
+logs_path = os.path.join(os.getcwd(), "logs", log_file_folder)
 os.makedirs(logs_path, exist_ok=True)
 
-log_file_path = os.path.join(logs_path, LOG_FILE)
+log_file_path = os.path.join(logs_path, log_file)
 
 logging.basicConfig(
     filename=log_file_path,
