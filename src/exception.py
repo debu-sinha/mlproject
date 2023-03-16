6bbc646e2d4512591d98cedd37ced1f564ec077b
Debu Sinha <debusinha2009@gmail.com>
2023-03-16 18:35:49 -0400

    removed debugging statement

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+
+def error_handler(err, err_detail: sys):
+    '''
+    This function is used to handle error and return the error message
+    '''
+    _, _, exec_tb = err_detail.exc_info()
+    filename = exec_tb.tb_frame.f_code.co_filename
+    err_message = f"Error: {filename} at line {exec_tb.tb_lineno} error message {err}"
+    return err_message
+    
+class CustomException(Exception):
+    '''
+    This class is used to handle custom exception
+    '''
+    def __init__(self, err, err_detail: sys):
+        super().__init__(err)
+        self.err_detail = error_handler(err, err_detail)
+    
+    def __str__(self):
+        return self.err_detail
+    
---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+
+current_time = datetime.now()
+log_file_folder = current_time.strftime("%Y_%m_%d")
+log_file = f"{current_time.strftime('%H')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE_FOLDER)
+os.makedirs(logs_path, exist_ok=True)
+
+log_file_path = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
